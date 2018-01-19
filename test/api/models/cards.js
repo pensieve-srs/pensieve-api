ec8dd8f737184a6289b84f57635470a2feb83547
nlaz <nikolazaris@utexas.edu>
2018-01-19 15:23:28 -0500

    add new tests

--- test/api/models/cards.js
+++ test/api/models/cards.js
@@ -7,63 +7,55 @@ const {
 
 describe('Card model', () => {
   describe('get', () => {
-    it('should return single card for user', (done) => {
-      Card.get(cards[0]._id, user1).then((card) => {
-        expect(card._id).to.deep.equal(cards[0]._id);
-        expect(card.front).to.deep.equal(cards[0].front);
-
-        done();
-      });
+    it('should return single card for user', async () => {
+      const card = await Card.get(cards[0]._id, user1);
+      expect(card._id).to.deep.equal(cards[0]._id);
+      expect(card.front).to.deep.equal(cards[0].front);
+    });
+  });
+  describe('getAll', () => {
+    it('should return all cards for user', async () => {
+      const response = await Card.getAll(user1);
+      expect(response).to.have.lengthOf(2);
+    });
+  });
+  describe('countAll', () => {
+    it('should return the length of all cards for a user', async () => {
+      const response = await Card.countAll(user1);
+      expect(response).to.equal(2);
     });
   });
   describe('create', () => {
-    it('should create single card for user', (done) => {
+    it('should create single card for user', async () => {
       const newCard = { front: 'New card front', back: 'New card back', deck: deck1 };
-      Card.create(newCard, user1).then((card) => {
-        expect(card.front).to.equal(newCard.front);
-        expect(card.back).to.equal(newCard.back);
-        expect(card.deck).to.deep.equal(deck1);
-
-        done();
-      });
+      const card = await Card.create(newCard, user1);
+      expect(card.front).to.equal(newCard.front);
+      expect(card.back).to.equal(newCard.back);
+      expect(card.deck).to.deep.equal(deck1);
     });
   });
   describe('update', () => {
-    it('should update single card for user', (done) => {
+    it('should update single card for user', async () => {
       const newCard = { front: 'New card front', back: 'New card back' };
-      Card.update(cards[0]._id, newCard, user1).then((card) => {
-        expect(card.front).to.equal(newCard.front);
-        expect(card.back).to.equal(newCard.back);
-
-        done();
-      });
+      const card = await Card.update(cards[0]._id, newCard, user1);
+      expect(card.front).to.equal(newCard.front);
+      expect(card.back).to.equal(newCard.back);
     });
   });
   describe('delete', () => {
-    it('should delete single card for user', (done) => {
-      Card.getAll(user1).then((prevCards) => {
-        Card.delete(prevCards[0]._id, user1).then(() => {
-          Card.getAll(user1).then((newCards) => {
-            expect(newCards).to.have.lengthOf(prevCards.length - 1);
-
-            done();
-          });
-        });
-      });
+    it('should delete single card for user', async () => {
+      const prevCards = await Card.getAll(user1);
+      await Card.delete(prevCards[0]._id, user1);
+      const newCards = await Card.getAll(user1);
+      expect(newCards.length).to.equal(prevCards.length - 1);
     });
   });
   describe('deleteAllByDeck', () => {
-    it('should delete all cards for a deck', (done) => {
-      Card.getAllByDeck(deck3, user2).then((prevCards) => {
-        Card.deleteAllByDeck(deck3, user2).then(() => {
-          Card.getAllByDeck(deck3, user2).then((newCards) => {
-            expect(prevCards).to.not.be.empty;
-            expect(newCards).to.be.empty;
-
-            done();
-          });
-        });
-      });
+    it('should delete all cards for a deck', async () => {
+      const prevCards = await Card.getAllByDeck(deck3, user2);
+      await Card.deleteAllByDeck(deck3, user2);
+      const newCards = await Card.getAllByDeck(deck3, user2);
+      expect(newCards.length).to.equal(prevCards.length - 1);
     });
   });
 });
