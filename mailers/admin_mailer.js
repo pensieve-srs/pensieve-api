24cdaa2d8cadd48c8566aa5bc6cafad4b65ec8da
nlaz <nikolazaris@utexas.edu>
2018-02-13 20:36:45 -0500

    add new job for expired decks

--- mailers/admin_mailer.js
+++ mailers/admin_mailer.js
@@ -1,5 +1,6 @@
 const text = require('./views/signup_alert.text.js');
 const mailer = require('@sendgrid/mail');
+
 const { classes: { EmailAddress } } = require('@sendgrid/helpers');
 
 mailer.setApiKey(process.env.SENDGRID_API_KEY);
@@ -7,10 +8,11 @@ mailer.setApiKey(process.env.SENDGRID_API_KEY);
 const adminEmail = process.env.ADMIN_EMAIL;
 const adminAddress = new EmailAddress({ name: 'Pensieve', email: adminEmail });
 
-module.exports.sendSignupAlert = () => mailer.send({
-  to: adminAddress,
-  from: adminAddress,
-  subject: 'New user alert! - You receive a new signup',
-  text: text(),
-  html: text(),
-});
+module.exports.sendSignupAlert = () =>
+  mailer.send({
+    to: adminAddress,
+    from: adminAddress,
+    subject: 'New user alert! - You receive a new signup',
+    text: text(),
+    html: text(),
+  });
